61914c51bac58699bcb934ba8fcc8bcbda24365f
jake spangler <jakespan@icloud.com>
2026-04-28 20:01:10 -0400

    switched back to supabase db

--- server/services/db/postgres.js
+++ server/services/db/postgres.js
@@ -2,7 +2,10 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 export async function savePlaylist({ playlistName, spotifyPlaylistUrl, songs }) {
   const { rows } = await pool.query(
